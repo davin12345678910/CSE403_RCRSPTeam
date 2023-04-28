4dbf8fe5e1dae77fe478e1f51147be0432561ffc
Foad Shariat <fshariat@uw.edu>
2023-04-27 19:55:13 -0700

    Added an empty registration page

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
+import RegistrationPage from './RegistrationPage';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                {/* Add more routes for other pages here */}
+                <Route path="/register" element={<RegistrationPage />} /> {/* Add this line */}
             </Routes>
         </Router>
     );
