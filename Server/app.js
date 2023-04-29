0c3fb59a01e781dc18bce2ea5005183755e779d8
davin123 <74631795+davin12345678910@users.noreply.github.com>
2023-04-29 00:40:41 -0700

    "added in server code"

--- Server/app.js
+++ Server/app.js
@@ -2,3 +2,17 @@
  * Contributors: Ahmed Helow, Azaan Khalfe, Chairnet Tegegne Muche, Davin Win Kyi,
  * Foad Shariat, and Sol Zamora.
  */
+
+
+"use strict";
+
+const express = require('express');
+const app = express();
+
+app.get('/posts', function (req, res) {
+  res.type("text").send("pussy");
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
