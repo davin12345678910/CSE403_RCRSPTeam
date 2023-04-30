3117625b9663a76ff3def26d465d60506914fee1
davin123 <74631795+davin12345678910@users.noreply.github.com>
2023-04-29 23:16:35 -0700

    "new changes"

--- Server/app_test.js
+++ Server/app_test.js
@@ -1,17 +1,25 @@
-const testAdd = require('./testAdd')
+// const testAdd = require('./testAdd');
+const { default: fetch } = await import('node-fetch');
 const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 
+
+
 async function getDBConnection() {
   const db = await sqlite.open({
-    filename: "registration.db",
+    filename: "registration_test.db",
     driver: sqlite3.Database
   });
 
   return db;
 }
 
-test('test adding to sql', () => {
+test('test adding to sql', async () => {
+  fetch('http://localhost:3000/posts')
+    .then(console.log);
+
+
+/*
   var database = await getDBConnection();
   let qry_test = adding;
 
@@ -21,5 +29,5 @@ test('test adding to sql', () => {
   let qry2 = 'SELECT* FROM classes WHERE class_id = 123;';
   let classes = await database.all(qry2);
   expect(testAdd('INSERT INTO classes ("class_id", "credits", "rating", "average_gpa", "professor", "assistant_professor", "class_times") VALUES ("123", 5, 3.5, 3.5, "nigiri", "pokimaine", "11:20-5:30");').toBe(classes))
-  
-}) 
+*/
+})
