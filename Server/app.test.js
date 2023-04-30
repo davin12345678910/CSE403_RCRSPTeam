eee9b5b5079886f2a8c125fcdaa57bcc7faa8012
Azaan <azaank@uw.edu>
2023-04-29 23:36:36 -0700

    updated test

--- Server/app.test.js
+++ Server/app.test.js
@@ -1,25 +1,41 @@
+import supertest from 'supertest'
+import app from './app.js'
+
 const testAdd = require('./testAdd')
 const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 
+describe("GET /posts", () => {
+  describe("I hate phedos", () => {
+    test("should have 200 status code", () => {
+      const response = await request(app).get("/posts")
+      console.log(response)
+    })
+  })
+  describe("PHEDO ERROR". () => {
+
+  })
+})
+
+
 async function getDBConnection() {
   const db = await sqlite.open({
-    filename: "registration_test.db",
+    filename: "registration.db",
     driver: sqlite3.Database
   });
 
   return db;
 }
 
-test('test adding to sql', () => {
-  var database = await getDBConnection();
-  let qry_test = adding;
+// test('test adding to sql', () => {
+//   var database = await getDBConnection();
+//   let qry_test = adding;
 
 //   let qry = 'INSERT INTO classes ("class_id", "credits", "rating", "average_gpa", "professor", "assistant_professor", "class_times") VALUES ("123", 5, 3.5, 3.5, "nigiri", "pokimaine", "11:20-5:30");';
 //   //await database.run(qry);
 
-  let qry2 = 'SELECT* FROM classes WHERE class_id = 123;';
-  let classes = await database.all(qry2);
-  expect(testAdd('INSERT INTO classes ("class_id", "credits", "rating", "average_gpa", "professor", "assistant_professor", "class_times") VALUES ("123", 5, 3.5, 3.5, "nigiri", "pokimaine", "11:20-5:30");').toBe(classes))
+//   let qry2 = 'SELECT* FROM classes WHERE class_id = 123;';
+//   let classes = await database.all(qry2);
+//   expect(testAdd('INSERT INTO classes ("class_id", "credits", "rating", "average_gpa", "professor", "assistant_professor", "class_times") VALUES ("123", 5, 3.5, 3.5, "nigiri", "pokimaine", "11:20-5:30");').toBe(classes))
   
-}) 
+// }) 
