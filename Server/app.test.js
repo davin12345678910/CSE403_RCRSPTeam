40e855e44cb0de87c5eb7f6289e09cf87d7f118a
davin123 <74631795+davin12345678910@users.noreply.github.com>
2023-04-30 23:39:35 -0700

    "testing infrastructure works now!"

--- Server/app.test.js
+++ Server/app.test.js
@@ -1,41 +1,18 @@
-import supertest from 'supertest'
+import request from 'supertest'
 import app from './app.js'
 
-const testAdd = require('./testAdd')
-const sqlite3 = require('sqlite3');
-const sqlite = require('sqlite');
 
-describe("GET /posts", () => {
-  describe("I hate phedos", () => {
-    test("should have 200 status code", () => {
-      const response = await request(app).get("/posts")
-      console.log(response)
+describe("POST /users", () => {
+  describe("given a username and password", () => {
+
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/users").send()
+      expect(response.statusCode).toBe(200)
+    })
+
+    test("should respond should be happy", async () => {
+      const response = await request(app).post("/users").send()
+      expect(response.body.name).toBe("happy")
     })
   })
-  describe("PHEDO ERROR". () => {
-
-  })
 })
-
-
-async function getDBConnection() {
-  const db = await sqlite.open({
-    filename: "registration.db",
-    driver: sqlite3.Database
-  });
-
-  return db;
-}
-
-// test('test adding to sql', () => {
-//   var database = await getDBConnection();
-//   let qry_test = adding;
-
-//   let qry = 'INSERT INTO classes ("class_id", "credits", "rating", "average_gpa", "professor", "assistant_professor", "class_times") VALUES ("123", 5, 3.5, 3.5, "nigiri", "pokimaine", "11:20-5:30");';
-//   //await database.run(qry);
-
-//   let qry2 = 'SELECT* FROM classes WHERE class_id = 123;';
-//   let classes = await database.all(qry2);
-//   expect(testAdd('INSERT INTO classes ("class_id", "credits", "rating", "average_gpa", "professor", "assistant_professor", "class_times") VALUES ("123", 5, 3.5, 3.5, "nigiri", "pokimaine", "11:20-5:30");').toBe(classes))
-  
-// }) 
