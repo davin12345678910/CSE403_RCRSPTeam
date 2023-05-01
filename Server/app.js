214c6b35272b196148a3ac6f2c84c736a47bea8b
davin123 <74631795+davin12345678910@users.noreply.github.com>
2023-05-01 00:11:06 -0700

    "databases now work!"

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,26 @@
-import express from 'express'
+'use strict';
 
+// These are the imports that we will require
+import express from 'express'
+import sqlite3 from 'sqlite3'
 const app = express()
+
+
+// This is how we will get the database that is within our system
+async function getDBConnection() {
+  const db = new sqlite3.Database("./registration.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message)
+  })
+  return db;
+}
+
+async function makeTables() {
+
+}
+
+
+
+getDBConnection();
 
 app.use(express.json())
 app.post('/users', async (req, res) => {
