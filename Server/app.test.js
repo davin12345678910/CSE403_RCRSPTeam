93bd42121ff4ef0ce03f1c02f3b3380473eebd7c
Azaan Khalfe <azaank@uw.edu>
2023-05-01 15:12:51 -0700

    updated timeouts

--- Server/app.test.js
+++ Server/app.test.js
@@ -1,31 +1,30 @@
 import request from 'supertest'
 import app from './app.js'
 
-
-
 describe("POST /users", () => {
   describe("given a username and password", () => {
 
     test("should respond with a 200 status code", async () => {
       const response = await request(app).post("/users").send()
       expect(response.statusCode).toBe(200)
-    })
+    }, 10000) // set timeout to 10 seconds
 
     test("Should contain 345", async () => {
       const addResponse = await request(app).post("/getClasses").send()
       var classes = addResponse.body.class
 
-      setTimeout(() => {
-
-      }, 7000000);
-
-      var found = false
-      classes.forEach(element => {
-        if (element.class_id == '345') {
-          found = true
-        }
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          var found = false
+          classes.forEach(element => {
+            if (element.class_id == '345') {
+              found = true
+            }
+          })
+          expect(found).toBe(true)
+          resolve()
+        }, 7000) // set timeout to 7 seconds
       })
-      expect(found).toBe(true)
-    })
+    }, 10000) // set timeout to 10 seconds
   })
 })
