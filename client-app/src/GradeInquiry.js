02c2a9b61dc7bde57a606955ed503ed54103be4c
CTM <ctm23@uw.edu>
2023-05-05 14:35:06 -0700

    updated the GradeInquiry.js

--- client-app/src/GradeInquiry.js
+++ client-app/src/GradeInquiry.js
@@ -2,6 +2,19 @@ const GradeInquiry = () => {
     return (
        <div>
             <h1>Grade Inquiry - Spring 2023</h1>
+            <select name="quarter" id="quarter">
+                <p> 
+                Select a different quarter to report:
+                </p>
+               
+                <option value="autumn">Autumn 2023</option>
+                <option value="winter">Winter 2024</option>
+                <option value="spring">Spring 2024</option>
+                <option value="summer">Summer 2024</option>
+                
+                
+            </select>
+            <button>Submmit</button>
             <div>
         
        </div>
