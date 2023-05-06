30bfd530822d99941fd19373c218fd6c7245b361
CTM <ctm23@uw.edu>
2023-05-05 20:20:04 -0700

    updated tuition statement page

--- client-app/src/TuitionStatement.js
+++ client-app/src/TuitionStatement.js
@@ -1,7 +1,12 @@
 const TuitionStatement = () => {
     return (
         <div>
-            <h1>Tuition Statement</h1>
+            <h1>Official Tuition Charge Statement - Spring 2023</h1>
+            <div>
+                <p> Student Name:	Name</p>
+                <p>Student Number: 1223333</p>
+                <p>Date prepared:	May 5, 2023</p>
+            </div>
         </div>
     )
 }
