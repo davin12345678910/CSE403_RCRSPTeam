f275cf7c8b5d2b29c0fd0afc943ba0452db8b188
CTM <ctm23@uw.edu>
2023-05-05 20:42:31 -0700

    updated visual schedule page

--- client-app/src/VisualSchedule.js
+++ client-app/src/VisualSchedule.js
@@ -5,6 +5,9 @@ const VisualSchedule = () => {
             <div> 
                 <p> Select a different quarter: quarter</p>  
                 {/* <p> Select a different quarter: </p>  this has to be a hiper link */}
+                <p>Prepared for: Name of the student</p>
+
+                {/* // this is the table that should be displayed */}
             
             </div>        
        
