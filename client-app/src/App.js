91e78da7095484496befc68eb8afe43744772639
Foad Shariat <fshariat@uw.edu>
2023-05-06 12:41:19 -0700

    added uw logo

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -15,28 +15,33 @@ import DirectDepositTransfer from './DirectDepositTransfer';
 import ShortTermLoan from './ShortTermLoan';
 import TuitionStatement from './TuitionStatement';
 import CreditCardPayment from './CreditCardPayment';
+import logo from './assets/wlogo.png';
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/register" element={<RegistrationPage />} />
-                <Route path="/schedule" element={<ClassSchedule />} />
-                <Route path="/visual" element={<VisualSchedule />} />
-                <Route path="/grade" element={<GradeInquiry />} />
-                <Route path ="/transcript" element={<UnofficialTranscript />} />
-                <Route path ="/official" element={<OfficalTranscript />} />
-                <Route path ="/audit" element={<DegreeAudit />} />
-                <Route path ="/address" element={<AddressandConsent />} />
-                <Route path ="/insurance" element={<InsuranceorOptionalCharges />} />
-                <Route path ="/financial" element={<FinancialAidStatus />} />
-                <Route path ="/direct" element={<DirectDepositTransfer />} />
-                <Route path ="/short" element={<ShortTermLoan />} />
-                <Route path ="/tuition" element={<TuitionStatement />} />
-                <Route path ="/credit" element={<CreditCardPayment />} />
-                
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <img src={logo}></img>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/register" element={<RegistrationPage />} />
+                    <Route path="/schedule" element={<ClassSchedule />} />
+                    <Route path="/visual" element={<VisualSchedule />} />
+                    <Route path="/grade" element={<GradeInquiry />} />
+                    <Route path ="/transcript" element={<UnofficialTranscript />} />
+                    <Route path ="/official" element={<OfficalTranscript />} />
+                    <Route path ="/audit" element={<DegreeAudit />} />
+                    <Route path ="/address" element={<AddressandConsent />} />
+                    <Route path ="/insurance" element={<InsuranceorOptionalCharges />} />
+                    <Route path ="/financial" element={<FinancialAidStatus />} />
+                    <Route path ="/direct" element={<DirectDepositTransfer />} />
+                    <Route path ="/short" element={<ShortTermLoan />} />
+                    <Route path ="/tuition" element={<TuitionStatement />} />
+                    <Route path ="/credit" element={<CreditCardPayment />} />
+
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 };
 
