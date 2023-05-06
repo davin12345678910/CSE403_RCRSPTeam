e762b95836beb16d70a560060487b363895620e2
CTM <ctm23@uw.edu>
2023-05-05 20:35:07 -0700

    updated InsuranceorOptionalCharges.js page

--- client-app/src/InsuranceorOptionalCharges.js
+++ client-app/src/InsuranceorOptionalCharges.js
@@ -1,7 +1,12 @@
 const InsuranceorOptionalCharges = () => {
     return (
         <div>
-            <h1>Insurance/Optional Charges</h1>
+            <h1>Insurance/Optional Charges - Autumn 2023</h1>
+            <div>
+            <p> Select a different quarter: Summer 2023 </p> 
+            {/* the "summer 2023"should be a hiper link */}
+            </div>
+            
         </div>
     );
 }
